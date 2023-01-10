411002daec600b718097a737178747ef32cdb11b
andreyrosa <contatomainort@gmail.com>
2023-01-10 10:08:17 -0300

    email infield, social media links

--- js/script.js
+++ js/script.js
@@ -1,16 +1,38 @@
 window.addEventListener("scroll", reveal);
 
 function reveal() {
-  let reveals = document.querySelectorAll('.reveal')
+  let reveals = document.querySelectorAll(".reveal");
 
-	for(let i = 0; i < reveals.length; i++) {
+  for (let i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let revealTop = reveals[i].getBoundingClientRect().top;
+    let revealPoint = 150;
 
-		let windowHeight = window.innerHeight;
-		let revealTop = reveals[i].getBoundingClientRect().top;
-		let revealPoint = 150;
-
-		if(revealTop < windowHeight - revealPoint) {
-			reveals[i].classList.add('active');
-		}
-	}
+    if (revealTop < windowHeight - revealPoint) {
+      reveals[i].classList.add("active");
+    }
+  }
 }
+
+const emailBtn = document.querySelector(".emailEnv");
+const emailBtnRemove = document.querySelector(".showEmailBtn");
+
+emailBtn.addEventListener("click", () => {
+  emailBtnRemove.classList.toggle("active");
+});
+
+
+const iconDiv = document.querySelector(".iconDiv");
+const textEmail = document.querySelector(".textEmail > p")
+
+iconDiv.onclick = function() {
+  document.execCommand("copy");
+}
+
+iconDiv.addEventListener("copy", function(event) {
+  event.preventDefault();
+  if (event.clipboardData) {
+    event.clipboardData.setData("text/plain", textEmail.textContent);
+    console.log(event.clipboardData.getData("text"))
+  }
+});
